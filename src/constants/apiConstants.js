82654853a5af24d20aa1bed977c99ed11dacee80
shilan4869 <104431403+shilan4869@users.noreply.github.com>
2022-11-12 17:39:40 +0700

    swiper and react bootstrap added

---
+++ src/constants/apiConstants.js
@@ -0,0 +1,6 @@
+export const TMDB_POSTER_ORIGIN = "https://image.tmdb.org/t/p/w500"
+export const TMDB_BACKDROP_ORIGIN = "https://image.tmdb.org/t/p/original"
+export const TMDB_MOVIE_ORIGIN = "https://api.themoviedb.org/3/movie"
+export const TMDB_MOVIE_SEARCH_ORIGIN =
+  "https://api.themoviedb.org/3/search/movie"
+export const TMDB_COLLECTION_ORIGIN = "https://api.themoviedb.org/3/collection"
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,34 @@
 import React from "react"
+import { Navigation, Mousewheel } from "swiper"
+import { Swiper, SwiperSlide } from "swiper/react"
+import "swiper/css"
+import "swiper/css/navigation"
 
-const index = () => {
-  return <h1 className="w-full min-h-10 bg-red-200 text-blue-900">Hello</h1>
+export default () => {
+  return (
+    <Swiper
+      // install Swiper modules
+      modules={[Navigation, Mousewheel]}
+      spaceBetween={50}
+      slidesPerView={2}
+      loop={true}
+      navigation
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log("slide change")}
+      className="w-full min-h-40 flex items-center bg-slate-500"
+    >
+      <SwiperSlide className="px-12" isDuplicate={true}>
+        Slide 1
+      </SwiperSlide>
+      <SwiperSlide className="px-12" isDuplicate={true}>
+        Slide 2
+      </SwiperSlide>
+      <SwiperSlide className="px-12" isDuplicate={true}>
+        Slide 3
+      </SwiperSlide>
+      <SwiperSlide className="px-12" isDuplicate={true}>
+        Slide 4
+      </SwiperSlide>
+    </Swiper>
+  )
 }
-
-export default index
