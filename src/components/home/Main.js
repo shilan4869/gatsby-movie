e4fb29b55de0e8e434f8f87a8693630661433da3
shilan4869 <104431403+shilan4869@users.noreply.github.com>
2022-12-13 14:33:17 +0700

    13/12

--- src/components/home/Main.js
+++ src/components/home/Main.js
@@ -2,14 +2,14 @@ import React from 'react'
 import TvShows from './TvShows'
 import Movies from './Movies'
 import Carousel from 'src/components/carousel/Carousel'
-import useAuthContext from 'src/hooks/useAuthContext'
+import useMenuTabContext from 'src/hooks/useMenuTabContext'
 
 
 const Main = ({ className }) => {
   const TV_TAB = 1
-  const { homepageTab } = useAuthContext()
+  const { menuTab } = useMenuTabContext
 
-  const MainContent = (homepageTab === TV_TAB) ? () => <TvShows /> : () => <Movies />
+  const MainContent = (menuTab === TV_TAB) ? () => <TvShows /> : () => <Movies />
 
   return (
     <div className={ className }>
